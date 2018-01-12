d5610e84ffe152aa6a66b5ee21b4fc36eed26666
NanoJoy <samuelevlafleur@gmail.com>
2018-01-11 23:11:19 -0500

    add acceleration for speedboosts for walking

--- src/Game.js
+++ src/Game.js
@@ -884,12 +884,12 @@ function GameState(game) {
             var speedBoost = null;
             var redDrag = null;
             curBoost = null;
-            for (var i = 0; i < arrays.speedBoosts.length; i++) {
-                speedBoost = arrays.speedBoosts[i];
-                game.physics.arcade.collide(this.sprite, speedBoost.sprite, function (playerSprite, speedSprite) {
-                    curBoost = speedBoost;
-                }, null, this);
-            }
+            // for (var i = 0; i < arrays.speedBoosts.length; i++) {
+            //     speedBoost = arrays.speedBoosts[i];
+            //     game.physics.arcade.collide(this.sprite, speedBoost.sprite, function (playerSprite, speedSprite) {
+            //         curBoost = speedBoost;
+            //     }, null, this);
+            // }
             this.touchingGround = false;
             game.physics.arcade.overlap(this.sprite, groups.grounds, function (playerSprite, groundSprite) {
                 if (groundSprite.key.indexOf("top") !== -1 || groundSprite.key.indexOf("d_both") !== -1) {
@@ -1211,13 +1211,21 @@ function GameState(game) {
                 console.log(difference);
                 impartedVelocity += difference * friction;
             }, onTop, this);
-            for (var i = 0; i < arrays.speedBoosts.length; i++) {
-                var boost = arrays.speedBoosts[i];
-                game.physics.arcade.collide(this.sprite, boost.sprite, function (playerSprite, blockSprite) {
-                    var vel = boost.direction === "left" ? -5 : 5;
-                    impartedVelocity += vel;
-                }, onTop, this);
-            }
+            var boosting = false;
+            arrays.speedBoosts.forEach(function (boost) {
+                var collided = false;
+                game.physics.arcade.collide(snail.sprite, boost.sprite, function (playerSprite, blockSprite) {
+                    if (Math.floor(playerSprite.bottom) === blockSprite.top && !boosting) {
+                        collided = true;
+                        var multiplier = boost.direction === "left" ? -1 : 1;                   
+                        boost.amount = boost.amount === 0 ? 15 * multiplier : boost.amount + 5 * multiplier;
+                        impartedVelocity += boost.amount;
+                    }
+                }, null, this);
+                if (!collided) {
+                    boost.amount = 0;
+                }
+            });
 
             this.sprite.body.velocity.x = runningSpeed + impartedVelocity;
             if (impartedVelocity > 0) {
@@ -1699,27 +1707,28 @@ function GameState(game) {
 
     function SpeedBoost(x, y, direction) {
         this.direction = direction;
+        this.amount = 0;
         this.sprite = groups.speedBoosts.create(x * 50, y * 50, "speedboost");
         game.physics.arcade.enable(this.sprite);
         this.sprite.body.moves = false;
         this.sprite.body.immovable = true;
-        if (direction === "left") {
-            this.sprite.scale.x = -1;
-            this.sprite.x -= this.sprite.width;
-        }
         var keyToAnims = {
             ground_top_1: [0, 1, 2, 3, 4],
             ground_top_2: [0, 1, 2, 3, 4],
             ground_bottom: [5, 6, 7, 8, 9],
-            ground_top_left: [15, 16, 17, 18, 19],
-            ground_top_right: [10, 11, 12, 13, 14],
+            ground_top_left: [10, 11, 12, 13, 14],
+            ground_top_right: [15, 16, 17, 18, 19],
             ground_both_left: [20, 21, 22, 23, 24],
             ground_both_right: [25, 26, 27, 28, 29],
             ground_top_both: [30, 31, 32, 33, 34],
             ground_both_both: [35, 36, 37, 38, 39]
         };
         var key = Snail.getGroundKey(x, y, map.layouts[areaNumber]);
-        console.log(keyToAnims[key]);
+        if (direction === "left") {
+            this.sprite.scale.x = -1;
+            this.sprite.x -= this.sprite.width;
+            key = key.replace("left", "right");
+        }
         this.sprite.animations.add("move", keyToAnims[key], 10, true);
         this.sprite.play("move");
     }
